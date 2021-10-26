ed76e040edbe35b7bdae7562f7410852dca297df
sumansaksh <suman_fw12_142@masai.school>
2021-10-26 13:06:32 +0530

    Create navbar.js
    food

---
+++ food app/components/navbar.js
@@ -0,0 +1,13 @@
+function navbar(){
+    
+    return  `
+    <div id="options">
+      <h3><a href="search.html">search Page</a></h3>
+      <h3><a href="day.html">Food of the day</a></h3>
+      <h3><a href="latest.html">Latest Recipes</a></h3>
+    </div>
+  
+   `
+}
+
+export default navbar
