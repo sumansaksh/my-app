e00bee27b2a0598879da35159989fe7488729e22
sumansaksh <suman_fw12_142@masai.school>
2021-11-24 02:08:44 +0530

    Update app.js

--- assignment/day2/app.js
+++ assignment/day2/app.js
@@ -7,12 +7,40 @@ const express = require('express');
 app.use(express.json());
 
 
- app.get("/", (req, res) => {
-    
-    res.send({books});
- })
+ const logger = (req, res, next)=>{
+   // console.log(req.method);
+   next();
+};
 
- app.post("/", (req, res) => {
+const authenticate = (req, res, next) => {
+   console.log("Authenticating");
+   next();
+ };
+
+let name = "suman";
+
+const authorise = (name) => {
+  return (req, res, next) => {
+    const originalSendFunc = res.send.bind(res);
+    res.send = (body) => {
+      body.api_requested_by = name;
+      return originalSendFunc(body);
+    };
+    next();
+  };
+};
+
+
+app.use(logger)
+
+
+
+
+app.get("/", authorise(name), (req, res) => {
+   res.send({ api_requested_by: "", books });
+ });
+
+ app.post("/",authorise(name), (req, res) => {
     
     const newUser = [...books,req.body]
 
@@ -21,6 +49,28 @@ app.use(express.json());
  });
 
 
+ app.get("/books/:id", authorise(name), (req, res) => {
+   const book = books.filter((book) => book.id === +req.params.id);
+   res.send({ api_requested_by: "", book });
+ });
+
+ app.patch("/books/:id", authorise(name), (req, res) => {
+  const {id} = req.params;
+  const {author, publissed_date} = req.body;
+  const book = books.find((book) => book.id === +id);
+  book.author = author;
+  book.publissed_date = publissed_date;
+  res.send({api_requested_by: "", books});
+});
+
+app.delete("/books/:id", authorise(name), (req, res) => {
+  const { id } = req.params;
+  const newBooks = books.filter((book) => book.id !== +req.params.id);
+  res.send({api_requested_by:"",newBooks});
+});
+
  app.listen(5432,function () {
      console.log("listening on port 5432");
  })
+
+ 
